34a1edcf80869aed6ef12d4993073bf023def952
DajmiPokoj <166950944+DajmiPokoj@users.noreply.github.com>
2025-04-09 22:47:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 function handleSubmit(e) {
-	e.preventDefault();
-	const form = document.getElementById("form")
-	const ves = form.querySelector("textarea").value;
-	const width = document.querySelector("div:nth-child(2)").clientWidth; 
-	const formular = new URLSearchParams(); 
-	formular.append('ves', ves);
+	e.preventDefault(); // zabrániť vstavenému odosielaniu v prehliadači
+
+	// this reprezentuje ten formular, ktory odosielame
+	const ves = this.querySelector("textarea").value; // Načítame text z textarea
+	const width = document.querySelector("section:nth-child(2)").clientWidth; // Načítame aktuálnu šírku výstupného okna
+
+	const formular = new URLSearchParams(); // Vytvoríme štruktúru, ktorá bude reprezentovať formulár
+	formular.append('ves', ves); // Pridáme tam naše hodnoty
 	formular.append('width', width);
 
-	const url = form.action; 
-	const method = form.method; 
-	fetch(url, {method: method, body: formular})
-		.then((res) => res.blob())
+	const url = this.action; // Nacitame povodnu URL zadanu vo formulari
+	const method = this.method; // NAcitame povodnu metodu zadanu vo formulari
+	fetch(url, {method: method, body: formular}) // Urobíme HTTP požiadavku na náš server POST /render a formularom v tele požiadavky 
+		.then((res) => res.blob()) // Dostali sme binárne dáta (blob)
 		.then((image) => {
-			document.querySelector("#output").src = URL.createObjectURL(image);
-			document.getElementById("output").style.visibility = "visible";
+			document.querySelector("#output").src = URL.createObjectURL(image); // Nastavíme src našeho <img> na načítaný obrázok
 		})
 }
-
-function clear(e) {
-    e.preventDefault();
-    document.querySelector("textarea").value = "VES v1.6 600 400";
-    document.getElementById("output").style.visibility = "hidden"; 
-}
+document.querySelector("form").addEventListener("submit", handleSubmit); // Nastavíme formulár, aby pri submit udalosti spustil našu handleSubmit funkciu
